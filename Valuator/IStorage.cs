10164da90e9df0aa4927b06f8e3daebc65c9ee2b
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2021-02-18 09:56:19 +0300

    lab1

---
+++ Valuator/IStorage.cs
@@ -0,0 +1,10 @@
+namespace Valuator
+{
+    public interface IStorage
+    {
+        void Store(string key, string value);
+
+        string Load(string key);
+        bool IsValueExist(string value);
+    }
+}
---
+++ Valuator/Pages/Index.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Valuator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IStorage _storage;
+
+        public IndexModel(ILogger<IndexModel> logger, IStorage storage)
+        {
+            _logger = logger;
+            _storage = storage;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost(string text)
+        {
+            _logger.LogDebug(text);
+
+            string id = Guid.NewGuid().ToString();
+
+            string rankKey = "RANK-" + id;
+            //TODO: посчитать rank и сохранить в БД по ключу rankKey
+            string rank;
+            if(text == null)
+            {
+                text = "";
+                rank = "0";
+            }
+            else
+            {
+                rank = ((float)text.Count(ch => char.IsLetter(ch)) / (float)text.Length).ToString();
+            }
+            _storage.Store(rankKey, rank);
+
+            string similarityKey = "SIMILARITY-" + id;
+            //TODO: посчитать similarity и сохранить в БД по ключу similarityKey
+            if(_storage.IsValueExist(text))
+            {
+                _storage.Store(similarityKey, "1");
+            }
+            else
+            {
+                _storage.Store(similarityKey, "0");
+            }
+
+            string textKey = "TEXT-" + id;
+            //TODO: сохранить в БД text по ключу textKey
+            _storage.Store(textKey, text);
+
+            return Redirect($"summary?id={id}");
+        }
+    }
+}
