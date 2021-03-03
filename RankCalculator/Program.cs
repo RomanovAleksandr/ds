25a3ea26b13b748610e03cc44aec4f1b9a074337
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2021-03-04 01:19:44 +0300

    lab3

--- RankCalculator/Program.cs
+++ RankCalculator/Program.cs
@@ -8,23 +8,38 @@ namespace RankCalculator
     {
         static void Main(string[] args)
         {
-            ConnectionFactory cf = new ConnectionFactory();
-            IConnection c = cf.CreateConnection();
-            EventHandler<MsgHandlerEventArgs> h = (sender, args) =>
+            // ConnectionFactory cf = new ConnectionFactory();
+            // IConnection c = cf.CreateConnection();
+            // EventHandler<MsgHandlerEventArgs> h = (sender, args) =>
+            // {
+            //     // print the message
+            //     Console.WriteLine(args.Message);
+
+            //     // Unsubscribing from within the delegate function is supported.
+            //     args.Message.ArrivalSubcription.Unsubscribe();
+            // };
+            // IAsyncSubscription s = c.SubscribeAsync("foo", h);
+            // //ISyncSubscription sSync = c.SubscribeSync("foo");
+            
+            // // Draining and closing a connection
+            // c.Drain();
+
+            // // Closing a connection
+            // c.Close();
+
+            void printMessage(object sender, MsgHandlerEventArgs e)
             {
-                // print the message
-                Console.WriteLine(args.Message);
+                System.Console.WriteLine(e.Message);
+            }
 
-                // Unsubscribing from within the delegate function is supported.
-                args.Message.ArrivalSubcription.Unsubscribe();
-            };
-            IAsyncSubscription s = c.SubscribeAsync("foo", h);
-
-            // Draining and closing a connection
-            c.Drain();
-
-            // Closing a connection
-            c.Close();
+            using (IConnection c = new ConnectionFactory().CreateConnection())
+            {
+                using (IAsyncSubscription s = c.SubscribeAsync("foo", printMessage))
+                {
+                    // Process for 5 seconds.
+                    //Thread.Sleep(5000);
+                }
+            }
         }
     }
 }
