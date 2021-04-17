8cb023d7e11a0f48626dd16af53e45a7c01969ee
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2021-04-17 20:55:54 +0300

    lab6

---
+++ Chat/Client/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        public static void StartClient(string host, int port, string message)
+        {
+            try
+            {
+                // Разрешение сетевых имён
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(host);
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                // CREATE
+                Socket sender = new Socket(
+                    ipAddress.AddressFamily,
+                    SocketType.Stream, 
+                    ProtocolType.Tcp);
+
+                try
+                {
+                    // CONNECT
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Удалённый адрес подключения сокета: {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Подготовка данных к отправке
+                    byte[] msg = Encoding.UTF8.GetBytes(message);
+
+                    // SEND
+                    int bytesSent = sender.Send(msg);
+
+                    // RECEIVE
+                    byte[] buf = new byte[1024];
+                    int bytesRec = sender.Receive(buf);
+
+                    Console.WriteLine("Ответ:\n{0}",
+                        Encoding.UTF8.GetString(buf, 0, bytesRec));
+
+                    // RELEASE
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                System.Console.WriteLine("Usage: Client <host> <port> <message>");
+                return;
+            }
+            
+            if (Int32.TryParse(args[1], out int port))
+            {
+                StartClient(args[0], port, args[2]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid port");
+                return;
+            }
+        }
+    }
+}
---
+++ Chat/Server/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Server
+{
+    class Program
+    {
+        public static void StartListening(int port)
+        {
+
+            // Разрешение сетевых имён
+            
+            // Привязываем сокет ко всем интерфейсам на текущей машинe
+            IPAddress ipAddress = IPAddress.Any; 
+            
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            // CREATE
+            Socket listener = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            List<string> messages = new List<string>();
+
+            try
+            {
+                // BIND
+                listener.Bind(localEndPoint);
+
+                // LISTEN
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Ожидание соединения клиента...");
+                    // ACCEPT
+                    Socket handler = listener.Accept();
+
+                    Console.WriteLine("Получение данных...");
+                    byte[] buf = new byte[1024];
+                    string data = null;
+                    do
+                    {
+                        int bytesRec = handler.Receive(buf);
+                        data += Encoding.UTF8.GetString(buf, 0, bytesRec);
+                    }
+                    while (handler.Available>0);
+
+                    Console.WriteLine("Полученный текст: {0}", data);
+                    messages.Add(data);
+
+                    // Отправляем текст обратно клиенту
+                    var result = String.Join("\n", messages.ToArray());
+                    byte[] msg = Encoding.UTF8.GetBytes(result);
+
+                    // SEND
+                    handler.Send(msg);
+
+                    // RELEASE
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine("Usage: Server <port>");
+                return;
+            }
+            
+            if (Int32.TryParse(args[0], out int port))
+            {
+                Console.WriteLine("Запуск сервера...");
+                StartListening(11000);
+            }
+            else
+            {
+                Console.WriteLine("Invalid port");
+                return;
+            }
+            
+            Console.WriteLine("\nНажмите ENTER чтобы выйти...");
+            Console.Read();
+        }
+    }
+}
