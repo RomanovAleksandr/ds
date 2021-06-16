cb998e06d10a04a739d0778d68808168e203b82d
RomanovAleksandr <61905953+RomanovAleksandr@users.noreply.github.com>
2021-06-16 15:50:33 +0300

    Update Program.cs

--- Chain/Program.cs
+++ Chain/Program.cs
@@ -66,6 +66,7 @@ namespace Chain
             {
                 Start(Int32.Parse(args[0]), args[1], Int32.Parse(args[2]), true);
             }
+            Console.ReadLine();
         }
         static void Connect(Socket sender, IPEndPoint remoteEP)
         {
